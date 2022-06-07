8b8ed140dfe9d353bb96bc52f85fc06eaba6b9bf
longerm <103499265+longerm@users.noreply.github.com>
2022-06-07 03:47:09 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-UUID=f6dde1a0-e876-4ecf-94c3-5fdeb2d43862
-WSPATH=/vless
+UUID=a46d1546-2e7a-4f7b-acd9-183eafbecea5
+WSPATH=/
 PORT=80
 
 # Write V2Ray configuration
